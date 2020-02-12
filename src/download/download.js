3a336d338226725c7e9180f727520737892620a9
ntibi <nicolastibi@yahoo.fr>
2020-02-12 15:08:15 +0100

    fix eslint

--- src/download/download.js
+++ src/download/download.js
@@ -2,8 +2,8 @@ const http = require("https");
 
 module.exports = (url) => new Promise ((resolve, reject) => {
   http.get(url, (res) => {
-    let body = ""
-    res.on("data", data => body += data)
-    res.on("end", () => resolve(body))
+    let body = "";
+    res.on("data", data => body += data);
+    res.on("end", () => resolve(body));
   }).on("error", (err) => reject(err)); 
 }); 
